cce79d078a8e577e53162eec8b97b701b6b03336
Clifford Yeboah <cliffordyeboah886@gmail.com>
2026-05-22 19:13:51 +0000

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'yeboah-engine-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './profile.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
